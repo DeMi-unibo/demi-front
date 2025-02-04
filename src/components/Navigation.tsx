24d9c32bfd5ef558dd476a71f3d194ece3a503f2
mjavadf <javad.me1376@gmail.com>
2025-02-04 15:46:50 +0100

    Fix navlinks

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,29 +1,29 @@
-import { 
-  Box, 
-  Flex, 
-  Spacer, 
-  Heading, 
-  Container 
-} from '@chakra-ui/react';
-import NavLink from './NavLink';
+import { Box, Flex, Spacer, Heading, Container } from "@chakra-ui/react";
+import NavLink from "./NavLink";
 
 function Navigation() {
-  return (
-    <Box bg="teal.500" py={4}>
-      <Container maxW="container.xl">
-        <Flex align="center">
-          <Heading color="white" size="md">DeMi Project</Heading>
-          <Spacer />
-          <Flex gap={3}>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/visualizations">Visualizations</NavLink>
-            <NavLink to="/metadata">Metadata</NavLink>
-            <NavLink to="/documentation">Documentation</NavLink>
-          </Flex>
-        </Flex>
-      </Container>
-    </Box>
-  );
+    return (
+        <Box bg="teal.500" py={4}>
+            <Container maxW="container.xl">
+                <Flex align="center">
+                    <Heading color="white" size="md">
+                        DeMi Project
+                    </Heading>
+                    <Spacer />
+                    <Flex gap={3}>
+                        <NavLink to="/demi-front/">Home</NavLink>
+                        <NavLink to="/demi-front/visualizations">
+                            Visualizations
+                        </NavLink>
+                        <NavLink to="/demi-front/metadata">Metadata</NavLink>
+                        <NavLink to="/demi-front/documentation">
+                            Documentation
+                        </NavLink>
+                    </Flex>
+                </Flex>
+            </Container>
+        </Box>
+    );
 }
 
 export default Navigation;
