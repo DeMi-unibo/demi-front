bc9cb571b807e2c3cd853d2b48968268bfdce2d1
mjavadf <javad.me1376@gmail.com>
2025-02-11 14:20:16 +0100

    Add SegmentedControl component for year selection in Visualizations

--- src/pages/Visualizations.tsx
+++ src/pages/Visualizations.tsx
@@ -1,13 +1,28 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import ChartMap from "../components/ChartMap";
+import { SegmentedControl } from "../components/ui/segmented-control";
+import { useState } from "react";
 
 function Visualizations() {
+    const years: string[] = ["2019", "2020", "2021", "2022", "2023"];
+    const [year, setYear] = useState("2019");
 
-  return (
-    <Box>
-      <ChartMap width="100%" height="500px" />
-    </Box>
-  );
+    return (
+        <Box width="100%" p={4}>
+            {/* Centered Year Selection Control */}
+            <Flex justify="center" mb={4}>
+                <SegmentedControl
+                    items={years}
+                    value={year}
+                    onValueChange={(value) => setYear(value.value)}
+                    colorPalette="pink"
+                />
+            </Flex>
+
+            {/* Map Visualization */}
+            <ChartMap width="100%" height="500px" year={year} />
+        </Box>
+    );
 }
 
 export default Visualizations;
